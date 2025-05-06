3efcd439e487d54e0cbf8c83a8b10f89a911df74
diarmacpro <164306202+diarmacpro@users.noreply.github.com>
2025-05-06 09:16:30 +0700

    Update bch.sh

--- bch.sh
+++ bch.sh
@@ -26,4 +26,16 @@ ip route get 1.1.1.1 | awk '{for(i=1;i<=NF;i++){if($i=="dev"){print "Interface  
 ip -o -4 addr show | awk '{print "IP Address      : " $4}'
 
 echo ""
+echo "===== Network Speedtest ====="
+if command -v speedtest >/dev/null 2>&1; then
+    speedtest --simple
+else
+    echo "Speedtest CLI belum terpasang. Menginstal..."
+    curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+    sudo apt install speedtest -y
+    echo "Menjalankan Speedtest..."
+    speedtest --simple
+fi
+
+echo ""
 echo "===== End of Report ====="
